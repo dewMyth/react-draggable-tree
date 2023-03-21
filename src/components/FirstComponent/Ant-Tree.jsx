5f159e46909d15e679812547c60605e19cf2691a
Dewmith Akalanka <akalankadewmith@gmail.com>
2023-03-21 16:48:33 +0530

    Add dummy data for tree

--- src/components/FirstComponent/Ant-Tree.jsx
+++ src/components/FirstComponent/Ant-Tree.jsx
@@ -1,122 +1,112 @@
-import { Tree } from "antd";
 import { useState } from "react";
-const x = 3;
-const y = 2;
-const z = 1;
-const defaultData = [];
-const generateData = (_level, _preKey, _tns) => {
-  const preKey = _preKey || "0";
-  const tns = _tns || defaultData;
-  const children = [];
-  for (let i = 0; i < x; i++) {
-    const key = `${preKey}-${i}`;
-    tns.push({
-      title: key,
-      key,
-    });
-    if (i < y) {
-      children.push(key);
-    }
-  }
-  if (_level < 0) {
-    return tns;
-  }
-  const level = _level - 1;
-  children.forEach((key, index) => {
-    tns[index].children = [];
-    return generateData(level, key, tns[index].children);
-  });
-};
-generateData(z);
+import { Tree } from "antd";
 
-const AntTree = ({ section }) => {
-  console.log(section);
+const { TreeNode } = Tree;
 
-  const [gData, setGData] = useState(defaultData);
-  const [expandedKeys] = useState(["0-0", "0-0-0", "0-0-0-0"]);
-  const onDragEnter = (info) => {
-    console.log(info);
-    // expandedKeys 需要受控时设置
-    // setExpandedKeys(info.expandedKeys)
-  };
+const data = [
+  {
+    title: "test 1",
+    key: "1",
+    children: [
+      {
+        title: "test 1.1",
+        key: "1-1",
+        children: [
+          {
+            title: "test 1.11",
+            key: "1-1-1",
+          },
+          {
+            title: "test 1.12",
+            key: "1-1-2",
+          },
+          {
+            title: "test 1.13",
+            key: "1-1-3",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "test 2",
+    key: "2",
+    children: [
+      {
+        title: "test 2.1",
+        key: "2-1",
+      },
+    ],
+  },
+  {
+    title: "test 3",
+    key: "3",
+    children: [
+      {
+        title: "test 3.1",
+        key: "3-1",
+      },
+    ],
+  },
+];
+
+function AntTree() {
+  const [treeData, setTreeData] = useState(data);
 
   const onDrop = (info) => {
-    console.log(info);
-    const dropKey = info.node.key;
-    const dragKey = info.dragNode.key;
-    const dropPos = info.node.pos.split("-");
-    const dropPosition =
-      info.dropPosition - Number(dropPos[dropPos.length - 1]);
-    const loop = (data, key, callback) => {
-      for (let i = 0; i < data.length; i++) {
-        if (data[i].key === key) {
-          return callback(data[i], i, data);
-        }
-        if (data[i].children) {
-          loop(data[i].children, key, callback);
-        }
-      }
-    };
-    const data = [...gData];
+    const { dropToGap, node, dragNode } = info;
 
-    // Find dragObject
-    let dragObj;
-    loop(data, dragKey, (item, index, arr) => {
-      arr.splice(index, 1);
-      dragObj = item;
-    });
-    if (!info.dropToGap) {
-      // Drop on the content
-      loop(data, dropKey, (item) => {
-        item.children = item.children || [];
-        // where to insert 示例添加到头部，可以是随意位置
-        item.children.unshift(dragObj);
-      });
-    } else if (
-      (info.node.props.children || []).length > 0 &&
-      // Has children
-      info.node.props.expanded &&
-      // Is expanded
-      dropPosition === 1 // On the bottom gap
-    ) {
-      loop(data, dropKey, (item) => {
-        item.children = item.children || [];
-        // where to insert 示例添加到头部，可以是随意位置
-        item.children.unshift(dragObj);
-        // in previous version, we use item.children.push(dragObj) to insert the
-        // item to the tail of the children
-      });
-    } else {
-      let ar = [];
-      let i;
-      loop(data, dropKey, (_item, index, arr) => {
-        ar = arr;
-        i = index;
-      });
-      if (dropPosition === -1) {
-        ar.splice(i, 0, dragObj);
-      } else {
-        ar.splice(i + 1, 0, dragObj);
+    // Find the parent node of the dragged node
+    const findParent = (data, key) => {
+      for (let i = 0; i < data.length; i++) {
+        const item = data[i];
+        if (item.key === key) {
+          return { data, index: i };
+        } else if (item.children) {
+          const result = findParent(item.children, key);
+          if (result) {
+            return result;
+          }
+        }
       }
-    }
-    setGData(data);
+      return null;
+    };
+
+    const dragNodeParent = findParent(treeData, dragNode.key);
+
+    // Remove the dragged node from its previous location
+    dragNodeParent.data.splice(dragNodeParent.index, 1);
+
+    // Find the new parent node and index
+    const dropNodeParent = findParent(treeData, node.key);
+
+    const dropIndex = dropToGap ? node.index : dropNodeParent.index + 1;
+
+    // Insert the dragged node into the new location
+    dropNodeParent.data.splice(dropIndex, 0, dragNode);
+
+    // Update the tree data state
+    setTreeData([...treeData]);
   };
+
+  const renderTreeNodes = (data) => {
+    return data.map((item) => {
+      if (item.children) {
+        return (
+          <TreeNode title={item.title} key={item.key} dataRef={item}>
+            {renderTreeNodes(item.children)}
+          </TreeNode>
+        );
+      }
+      return <TreeNode {...item} />;
+    });
+  };
+
   return (
-    <>
-      {section ? (
-        <Tree
-          className="draggable-tree"
-          defaultExpandedKeys={expandedKeys}
-          draggable
-          blockNode
-          onDragEnter={onDragEnter}
-          onDrop={onDrop}
-          treeData={gData}
-        />
-      ) : (
-        <div>Click a section to see strategies</div>
-      )}
-    </>
+    <Tree draggable onDrop={onDrop} treeData={treeData}>
+      {renderTreeNodes(treeData)}
+    </Tree>
   );
-};
+}
+
 export default AntTree;
